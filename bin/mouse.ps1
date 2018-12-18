353884a0a4bf427ddbea8942cf56c51377153aaf
Kied T L <eli.georgios@gmail.com>
2018-12-18 13:14:33 -0500

    Fixed mouse command

--- bin/mouse.ps1
+++ bin/mouse.ps1
@@ -6,10 +6,18 @@ set-strictmode -off
 . "..\lib\core.ps1"
 . (relpath '..\lib\commands')
 
+$nvurl = "https://raw.githubusercontent.com/Kiedtl/mouse/master/share/version.dat"
+
 $commands = commands
 if ('--version' -contains $cmd -or (!$cmd -and '-v' -contains $args)) {
-    Write-Host "Mouse: " -NoNewLine
-    Write-Host -f Green "v0.1.0"
+    Write-Host "Current Mouse Version: " -NoNewLine
+    $currver = Get-Content "$psscriptroot\..\share\version.dat"
+    Write-Host -f Green ("$currver")
+    if ((Test-Connection -site microsoft.com -count 1 -quiet)) {
+        $newver = (New-Object Net.WebClient).DownloadString($nvurl)
+        Write-Host "Latest Mouse Version: " -NoNewline
+        Write-Host ("$newver") -f Blue
+    }
 }
 elseif (@($null, '--help', '/?') -contains $cmd -or $args[0] -contains '-h') { exec 'help' $args }
 elseif ($commands -contains $cmd) { exec $cmd $args }
