51ed7f3f3cbe59cb774f51724b056c820f652dca
Kied T L <eli.georgios@gmail.com>
2018-12-18 14:31:43 -0500

    Add download and Get-UserAgent function to core file

--- lib/core.ps1
+++ lib/core.ps1
@@ -1,3 +1,7 @@
+function Get-UserAgent() {
+    $version = Get-Content ("$psscriptroot\..\share\version.dat")
+    return ("Mouse/$version (+http://mouse.projects.kiedtl.surge.sh/) PowerShell/$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) (Windows NT $([System.Environment]::OSVersion.Version.Major).$([System.Environment]::OSVersion.Version.Minor); $(if($env:PROCESSOR_ARCHITECTURE -eq 'AMD64'){'Win64; x64; '})$(if($env:PROCESSOR_ARCHITEW6432 -eq 'AMD64'){'WOW64; '})$PSEdition)")
+}
 function abort($msg, [int] $exit_code=1) { write-host $msg -f red; exit $exit_code }
 function error($msg) { write-host "ERROR $msg" -f darkred }
 function warn($msg) {  write-host "WARN  $msg" -f darkyellow }
@@ -14,3 +18,10 @@ function test_internet() {
     $conn = (Test-Connection -ComputerName google.com -Count 2 -Quiet)
     return $conn
 }
+function download($src, $dest) {
+    $wc = New-Object Net.Webclient
+    $wc.headers.add('Referer', (strip_filename $src))
+    $wc.Headers.Add('User-Agent', (Get-UserAgent))
+    $wc.downloadFile($url, $dest)
+}
+
