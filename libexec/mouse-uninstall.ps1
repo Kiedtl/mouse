57af0a80dbc8ee85869f02b3857943fc6e8bb311
Kied T L <eli.georgios@gmail.com>
2018-12-20 16:19:45 -0500

    Fixed bug

--- libexec/mouse-uninstall.ps1
+++ libexec/mouse-uninstall.ps1
@@ -1,3 +1,7 @@
+# Usage: mouse uninstall
+# Summary: Uninstalls Mouse from the local machine.
+# Help: Delete the local Mose directory and related data, but does not delete the remove repository.
+
 $UNINSTALL_SCRIPT = "$psscriptroot\..\bin\uninstall.ps1"
 
 $confirmation = Read-Host "Are you sure?? (y/N)"
