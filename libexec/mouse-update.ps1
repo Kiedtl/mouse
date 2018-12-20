b3d0c084ed0bb9a3abb2863d588f2f82ed1de371
Kied T L <eli.georgios@gmail.com>
2018-12-20 09:15:45 -0500

    Beautified update process

--- libexec/mouse-update.ps1
+++ libexec/mouse-update.ps1
@@ -6,18 +6,93 @@
 
 . "$psscriptroot\..\lib\core.ps1"
 
-spinner_sticks 10 80 "Updating Mouse..."
+$newver = dl_string $nvurl;
 $nvurl = "https://raw.githubusercontent.com/Kiedtl/mouse/master/share/version.dat"
 if (test_internet) {
-    $newver = dl_string $nvurl
-    Push-Location
-    Set-Location "$HOME/.mouse/app"
-    git pull origin master -q > "$psscriptroot\..\share\dump.tmp"
-    Set-Content -Path "share\version.dat" -Value $newver
-    git commit -a -q -m "Updated Mouse" > ..\app\share\dump.tmp
-    Write-Host " done" -f Green
-    success "Successfully updated Mouse to $newver`n"
-    Pop-Location
+    Push-Location;
+
+    $slp = Invoke-Command -ComputerName $env:ComputerName -ScriptBlock { start-sleep -s 2; } -AsJob
+    $sleeptime = 80;
+    $text = "Updating Mouse... "
+    
+    while (($slp.State -eq "Running") -and ($slp.State -ne "NotStarted"))
+    {
+        write-host "`r`r[ | ] $text" -NoNewline
+        start-sleep -m $sleeptime
+        write-host "`r`r[ / ] $text" -NoNewline
+        start-sleep -m $sleeptime
+        write-host "`r`r[ - ] $text" -NoNewline
+        start-sleep -m $sleeptime
+        write-host "`r`r[ \ ] $text" -NoNewline
+        start-sleep -m $sleeptime
+    }
+
+    $sleep = Invoke-Command -ComputerName $env:ComputerName -ScriptBlock { start-sleep -s 5; } -AsJob
+    $sleeptime = 80;
+    $text = "Updating Mouse... initializing"
+    
+    while (($sleep.State -eq "Running") -and ($sleep.State -ne "NotStarted"))
+    {
+        write-host "`r`r[ | ] $text" -NoNewline
+        start-sleep -m $sleeptime
+        write-host "`r`r[ / ] $text" -NoNewline
+        start-sleep -m $sleeptime
+        write-host "`r`r[ - ] $text" -NoNewline
+        start-sleep -m $sleeptime
+        write-host "`r`r[ \ ] $text" -NoNewline
+        start-sleep -m $sleeptime
+    }
+
+    $getstring = Invoke-Command -ComputerName $env:ComputerName -ScriptBlock { $newver = dl_string $nvurl; Set-Location "$HOME/.mouse/app"; } -AsJob
+    $sleeptime = 80;
+    $text = "Updating Mouse... retrieving online data"
+    
+    while (($getstring.State -eq "Running") -and ($getstring.State -ne "NotStarted"))
+    {
+            write-host "`r`r[ | ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ / ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ - ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ \ ] $text" -NoNewline
+            start-sleep -m $sleeptime
+    }
+    
+    $gitpull = Invoke-Command -ComputerName $env:ComputerName -ScriptBlock { git pull origin master -q > "$psscriptroot\..\share\dump.tmp"; } -AsJob
+    $text = "Updating Mouse... pulling from origin/master"
+    
+    while (($gitpull.State -eq "Running") -and ($gitpull.State -ne "NotStarted"))
+    {
+            write-host "`r`r[ | ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ / ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ - ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ \ ] $text" -NoNewline
+            start-sleep -m $sleeptime
+    }
+    
+    $savechanges = Invoke-Command -ComputerName $env:ComputerName -ScriptBlock { Set-Content -Path "share\version.dat" -Value $newver; git commit -a -q -m "Updated Mouse" > ..\app\share\dump.tmp } -AsJob
+    $text = "Updating Mouse... commiting changes          "
+    
+    while (($savechanges.State -eq "Running") -and ($savechanges.State -ne "NotStarted"))
+    {
+            write-host "`r`r[ | ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ / ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ - ] $text" -NoNewline
+            start-sleep -m $sleeptime
+            write-host "`r`r[ \ ] $text" -NoNewline
+            start-sleep -m $sleeptime
+    }
+    
+        Write-Host "`r`r[ - ] Updating Mouse.... " -NoNewline
+        Write-Host " done                     " -f Green
+        success "Successfully updated Mouse to $newver`n"
+        Pop-Location
 }
 else {
     Write-Host " error" -f Red
