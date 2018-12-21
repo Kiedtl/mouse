782f6beafd921bd9f95851e788bf361961f4e6a9
Kied T L <eli.georgios@gmail.com>
2018-12-21 09:49:59 -0500

    Fixed bug

--- libexec/mouse-uninstall.ps1
+++ libexec/mouse-uninstall.ps1
@@ -5,7 +5,10 @@
 $UNINSTALL_SCRIPT = "$psscriptroot\..\bin\uninstall.ps1"
 
 $confirmation = Read-Host "Are you sure?? (y/N)"
-$conf = $comfirmation.ToLower()
+if ($confimation -eq $null) {
+    break
+}
+$conf = $confirmation.ToLower()
 
 if (($conf -eq "yes") -or ($conf -eq "y")) {
     & $UNINSTALL_SCRIPT
