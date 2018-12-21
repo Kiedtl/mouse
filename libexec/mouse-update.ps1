98644a0d51784fec95193ff890124e79f7832410
Kied T L <eli.georgios@gmail.com>
2018-12-21 10:46:50 -0500

    Fixed bug

--- libexec/mouse-update.ps1
+++ libexec/mouse-update.ps1
@@ -9,38 +9,23 @@ $newver = dl_string $nvurl;
 $nvurl = "https://raw.githubusercontent.com/Kiedtl/mouse/master/share/version.dat"
           
 if (test_internet) {
+    spinner_sticks 10 80 "Updating Mouse..."
     Push-Location;
+    $newver = dl_string $nvurl;
+    Set-Location "$HOME/.mouse/app";
+    git pull origin master > $psscriptroot\..\share\dump.tmp
+    Set-Content -Path "share\version.dat" -Value $newver;
+    git commit -a -q -m "Updated Mouse" > ..\app\share\dump.tmp;
 
-    $updater = Invoke-Command -ComputerName $env:ComputerName -ScriptBlock {
-        $newver = dl_string $nvurl;
-        Set-Location "$HOME/.mouse/app";
-        git pull origin master  > "$psscriptroot\..\share\dump.tmp";
-        Set-Content -Path "share\version.dat" -Value $newver;
-        git commit -a -q -m "Updated Mouse" > ..\app\share\dump.tmp;
-    } -AsJob
-
-    $sleeptime = 80;
-    $text = "Updating Mouse..."
-    
-    while (($updater.State -eq "Running") -and ($updater.State -ne "NotStarted"))
-    {
-            write-host "`r`r[ | ] $text" -NoNewline
-            start-sleep -m $sleeptime
-            write-host "`r`r[ / ] $text" -NoNewline
-            start-sleep -m $sleeptime
-            write-host "`r`r[ - ] $text" -NoNewline
-            start-sleep -m $sleeptime
-            write-host "`r`r[ \ ] $text" -NoNewline
-            start-sleep -m $sleeptime
-    }
-        Write-Host "`r`r[ - ] Updating Mouse..." -NoNewline
-        Write-Host " done                        " -f Green
-        success "Successfully updated Mouse to $newver`n"
-        Pop-Location
+    Write-Host "`r`r[ - ] Updating Mouse..." -NoNewline
+    Write-Host " done                        " -f Green
+    success "Successfully updated Mouse to $newver`n"
+    Pop-Location
 }
 else {
-    Write-Host "[ - ] Updating Mouse..." -NoNewline
+    spinner_sticks 5 80 "Updating Mouse... "
     Start-Sleep -m 1000
     Write-Host " error" -f Red
     abort "mouse: ***** Unable to connect to the internet. Stop."
 }
+
