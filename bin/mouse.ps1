1ee7d6bbc86645231582b16c615fd8eccc67dea0
Kied T L <eli.georgios@gmail.com>
2018-12-24 13:16:23 -0500

    Fixed entrypoint

--- bin/mouse.ps1
+++ bin/mouse.ps1
@@ -10,6 +10,14 @@ set-strictmode -off
 $nvurl = "https://raw.githubusercontent.com/Kiedtl/mouse/master/share/version.dat"
 $FIGLET = "$psscriptroot\..\lib\figlet.exe"
 $SAY = "$psscriptroot\..\lib\say.ps1"
+$CRAFT = "$psscriptroot\..\lib\craft.exe"
+
+[int32]$update = Get-Random -Maximum 13
+if ((is_prime $update)) {
+    warn "Mouse is may be out of date."
+    warn "Mouse is being automatically updated."
+    mouse update
+}
 
 $commands = commands
 if ('--version' -contains $cmd -or (!$cmd -and '-v' -contains $args)) {
@@ -22,10 +30,34 @@ if ('--version' -contains $cmd -or (!$cmd -and '-v' -contains $args)) {
         Write-Host ("$newver") -f Blue
     }
 }
-elseif ('yay' -contains $cmd -or (!$cmd -and '--yay' -contains $args)) {
+elseif ('--craft' -contains $cms -or (!$cmd -and '-x' -contains $args)) {
+    if ($IsWindows) {
+        & $CRAFT
+    }
+    else {
+        warn "MouseCraft is compatible with Win32 systems only."
+        $yn = Read-Host "Start anyway? (y/N)"
+        if ($yn -notlike 'y*') {
+            break
+        }
+        else {
+            & $CRAFT
+        }
+    }
+}
+elseif ('--yay' -contains $cmd -or (!$cmd -and '-y' -contains $args)) {
     & $SAY yay!!! -v "Microsoft Zira Desktop" -r -5}
-elseif ('mouse' -contains $cmd -or (!$cmd -and '--mouse' -contains $args)) {
-    & $FIGLET -f small -c mouse
+elseif ('--mouse' -contains $cmd -or (!$cmd -and '-z' -contains $args)) {
+    if ($IsWindows) {
+        & $FIGLET -f small -c mouse
+    }
+    else {
+        "
+`t`t  _ __  ___ _  _ ___ ___
+`t`t | '  \/ _ \ || (_-</ -_)
+`t`t |_|_|_\___/\_,_/__/\___|
+"
+    }
 }
 elseif (@($null, '--help', '/?') -contains $cmd -or $args[0] -contains '-h') {
     exec 'help' $args
