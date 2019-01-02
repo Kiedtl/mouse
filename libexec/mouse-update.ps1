2d61f1ebcdab2ea0f2914490946f8247b722ba53
Kied T L <eli.georgios@gmail.com>
2019-01-01 19:26:33 -0500

    Fixed mouse-update

--- libexec/mouse-update.ps1
+++ libexec/mouse-update.ps1
@@ -4,9 +4,11 @@
 #       `git pull`.
 
 . "$psscriptroot\..\lib\core.ps1"
+. "$psscriptroot\..\lib\gitutils.ps1"
 
 $newver = dl_string $nvurl;
 $nvurl = "https://raw.githubusercontent.com/Kiedtl/mouse/master/share/version.dat"
+$branch = Get-GitBranch
           
 if (test_internet) {
     spinner_sticks 10 80 "Updating Mouse..."
@@ -14,19 +16,20 @@ if (test_internet) {
     $newver = dl_string $nvurl;
     Set-Location "$HOME/.mouse/app";
     git stash > $HOME/.mouse/dump.tmp
-    git pull origin master --quiet --force | Out-Null
+    git pull origin $branch --quiet --force | Out-Null
     Set-Content -Path "share\version.dat" -Value $newver;
     git commit -a -q -m "Updated Mouse" | Out-Null
 
     Write-Host "`r`r[ - ] Updating Mouse..." -NoNewline
     Write-Host " done                        " -f Green
-    success "Successfully updated Mouse to $newver`n"
+    success "Successfully updated Mouse."
     Pop-Location
 }
+
 else {
     spinner_sticks 5 80 "Updating Mouse... "
     Start-Sleep -m 1000
     Write-Host " error" -f Red
-    abort "mouse: ***** Unable to connect to the internet. Stop."
+    abort "mouse: Unable to update Mouse: no internet."
 }
 
