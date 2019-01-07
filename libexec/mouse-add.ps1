4d44d5f28ad98b48fdf50a91fb1c0eab8e15f3cc
Kied T L <eli.georgios@gmail.com>
2019-01-07 13:48:01 -0500

    Add ravenclient error integration to mouse-add.ps1 for issue for issue #012

--- libexec/mouse-add.ps1
+++ libexec/mouse-add.ps1
@@ -21,6 +21,20 @@ $TOUCH = ("$psscriptroot\..\lib\touch.ps1")
 $message = $opt.message -or $opt.m
 $nosync = $opt.nosync -or $opt.n
 
+trap {
+    . "$psscriptroot\..\lib\core.ps1"
+    warn "An error occurred in Mouse. "
+    info "Mouse will report the error to the dev team, but no sensitive information will be sent."
+    Write-Host "Reporting error... " -NoNewline
+    . "$psscriptroot\..\lib\ravenclient.ps1"
+    [uint64]$d_snn = 16850863275
+    [string]$directory_singular_nuisance = "https://c80867d30cd048ca9375d3e7f99e28a8:f426d337a9434aa7b7da0ec16166ca98@sentry.io/$($d_snn / 12345)"
+    $ravenClient = New-RavenClient -SentryDsn $dsn
+    $ravenClient.CaptureException($_)
+    Write-Host "done" -f Green
+}
+
+
 Push-Location
 Set-Location $HOME/.mouse/dat
 git-crypt unlock $HOME/.mouse/git_crypt_key.key
