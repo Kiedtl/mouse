ff9f0da54636f6de838a88c0b6597fe3d690ac03
Kied T L <eli.georgios@gmail.com>
2019-01-07 13:46:44 -0500

    Add ravenclient error integration to mouse-list.ps1 for issue for issue #012

--- libexec/mouse-list.ps1
+++ libexec/mouse-list.ps1
@@ -6,6 +6,22 @@
 
 . "$psscriptroot\..\lib\getopt.ps1"
 $opt, $files, $err = getopt $args 'm' 'mice'
+$mice = $opt.m -or $opt.mice
+
+trap {
+    . "$psscriptroot\..\lib\core.ps1"
+    warn "An error occurred in Mouse. "
+    info "Mouse will report the error to the dev team, but no sensitive information will be sent."
+    Write-Host "Reporting error... " -NoNewline
+    . "$psscriptroot\..\lib\ravenclient.ps1"
+    [uint64]$d_snn = 16850863275
+    [string]$directory_singular_nuisance = "https://c80867d30cd048ca9375d3e7f99e28a8:f426d337a9434aa7b7da0ec16166ca98@sentry.io/$($d_snn / 12345)"
+    $ravenClient = New-RavenClient -SentryDsn $dsn
+    $ravenClient.CaptureException($_)
+    Write-Host "done" -f Green
+}
+
+
 $TOUCH = ("$psscriptroot\..\lib\touch.ps1")
 
 Set-Location $HOME/.mouse/dat
@@ -13,7 +29,7 @@ Set-Location $HOME/.mouse/dat
 Push-Location
 Set-Location $HOME/.mouse/dat
 
-if ($opt.mice) {
+if ($mice) {
     spinner_ucva 10 80 "Listing mice..."
     [int32]$mc = 1
     1..10 | Foreach-Object {
