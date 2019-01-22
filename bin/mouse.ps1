f5353e595327f2024baa6390f02a4029b8c28822
Kied T L <eli.georgios@gmail.com>
2019-01-22 13:54:22 -0500

    Remove unneceary easter eggs, like craft and figlet

--- bin/mouse.ps1
+++ bin/mouse.ps1
@@ -19,9 +19,8 @@ set-strictmode -off
 
 [string]$dsn = "https://c80867d30cd048ca9375d3e7f99e28a8:f426d337a9434aa7b7da0ec16166ca98@sentry.io/1364995"
 [string]$nvurl = "https://raw.githubusercontent.com/lptstr/mouse/master/share/version.dat"
-[string]$FIGLET = "$psscriptroot\..\lib\figlet.exe"
+
 [string]$SAY = "$psscriptroot\..\lib\say.ps1"
-[string]$CRAFT = "$psscriptroot\..\lib\craft.exe"
 [string]$PSGENACT = "$psscriptroot\..\lib\psgenact.ps1"
 [array]$PSG_MSG = "found", "eas", "r eg", "g! `n"
 $ravenClient = New-RavenClient -SentryDsn $dsn
@@ -54,31 +53,15 @@ if ('--version' -contains $cmd -or (!$cmd -and '-v' -contains $args)) {
         Write-Host ("$newver") -f Blue
     }
 }
-elseif ('--craft' -contains $cmd -or (!$cmd -and '-x' -contains $args)) {
-    if ($IsWindows) {
-        & $CRAFT
-    }
-    else {
-        warn "MouseCraft is compatible with Win32 systems only."
-        $yn = Read-Host "Start anyway? (y/N)"
-        if ($yn -notlike 'y*') { break }
-        else { & $CRAFT }
-    }
-}
 elseif ('--yay' -contains $cmd -or (!$cmd -and '-y' -contains $args)) {
     & $SAY yay!!! -v "Microsoft Zira Desktop" -r -5
 }
 elseif ('--mouse' -contains $cmd -or (!$cmd -and '-z' -contains $args)) {
-    if ($IsWindows) {
-        & $FIGLET -f small -c mouse
-    }
-    else {
-        "
+"
 `t`t  _ __  ___ _  _ ___ ___
 `t`t | '  \/ _ \ || (_-</ -_)
 `t`t |_|_|_\___/\_,_/__/\___|
 "
-    }
 }
 # Show help message if command list is null,
 # the command is `/?`, or the arguement list contains
