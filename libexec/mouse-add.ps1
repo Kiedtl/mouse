425a79d5595d1b42c403eb2b9c16dd87419de1ab
Kied T Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-01-23 12:06:06 -0500

    Integrate logging information with mouse add command for issue #012

--- libexec/mouse-add.ps1
+++ libexec/mouse-add.ps1
@@ -9,6 +9,7 @@
 # Options:
 #   -m, --message               Use a custom Git commit message
 #   -n, --nosync                Don't synchronize repository with GitHub
+#   -l, --show-logs             Output additional logging information.
 
 Add-Type -assembly "System.IO.Compression.Filesystem"
 
@@ -16,7 +17,7 @@ Add-Type -assembly "System.IO.Compression.Filesystem"
 . "$psscriptroot\..\lib\getopt.ps1"
 . "$psscriptroot\..\lib\config.ps1"
 
-$opt, $files, $err = getopt $args 'm:n' 'message', 'nosync'
+$opt, $files, $err = getopt $args 'm:nl' 'message', 'nosync', 'show-logs'
 $TOUCH = ("$psscriptroot\..\lib\touch.ps1")
 $message = $opt.message -or $opt.m
 $nosync = $opt.nosync -or $opt.n
@@ -24,6 +25,8 @@ $nosync = $opt.nosync -or $opt.n
 trap {
     . "$psscriptroot\..\lib\core.ps1"
     warn "An error occurred in Mouse. "
+    log -level "error" -opt $opt -status 500 -msg "ADD: Error: $($_.ToString())"
+    log -level "error" -opt $opt -status 106 -msg "ADD: Reporting previous error"
     info "Mouse will report the error to the dev team, but no sensitive information will be sent."
     Write-Host "Reporting error... " -NoNewline
     . "$psscriptroot\..\lib\ravenclient.ps1"
@@ -37,27 +40,36 @@ trap {
 
 Push-Location
 Set-Location $HOME/.mouse/dat
+log -level "info" -opt $opt -status 100 -msg "Changing directory to $HOME/.mouse/dat"
 
 if ($true) { # TODO: Implement --plumbing flag and change $true to if !($plumbing)
     Write-Host "Unlocking repository..." -NoNewline
+    log -level "info" -opt $opt -status 108 -msg "ADD: Unlocking repository with key at $HOME/.mouse/git_crypt_key.key"
     git-crypt unlock $HOME/.mouse/git_crypt_key.key | out-null
     Write-Host " done" -f Green
 }
 
 Set-Location $HOME
+log -level "info" -opt $opt -status 100 -msg "ADD: Changing directory to $HOME"
 
 if ($err) {
     $err | ForEach-Object {
         error $err
+        log -level "error" -opt $opt -status 401 -msg "ADD: Argument error: $err"
         exit 1
     }
 }
+else {
+    log -level "success" -opt $opt -status 203 -msg "ADD: Successfully parsed arguments."
+}
 
 if (!$files) {
-    abort "mouse: ***** File or directory list not provided. Stop."
+    abort "mouse: File or directory list not provided."
+    log -level "error" -opt $opt -status 401 -msg "ADD: No directory or file list provided"
 }
 
 $files | ForEach-Object {
+    log -level "info" -opt $opt -status 101 -msg "ADD: Processing file or directory $_"
     $rawpath = (friendly_path (friendly_path $_))
     $_ = unfriendly_path $_
     $name = fname $_
@@ -68,6 +80,7 @@ $files | ForEach-Object {
     if ((Test-Path $_)) {
         $isDirectory = ((Get-Item $_) -is [System.IO.DirectoryInfo])
         if (!$isDirectory) {
+            log -level "info" -opt $opt -status 104 -msg "ADD: $_ is a file"
             if (Test-Path "$HOME/.mouse/dat/${name}")
             {
                 Remove-Item "$HOME/.mouse/dat/${name}"
@@ -85,7 +98,9 @@ $files | ForEach-Object {
             }
         }
         else {
+            log -level "info" -opt $opt -status 100 -msg "ADD: Item $_ is a directory"
             if (Test-Path $dirdest) {
+                log -level "warn" -opt $opt -status 301 -msg "ADD: Overwriting $dirdest"
                 Remove-Item $dirdest
                 warn "Overwriting $dirdest"
             }
@@ -119,7 +134,8 @@ $files | ForEach-Object {
 
     }
     else {
-        abort "mouse: ***** The file $_ does not exist or is hidden. Stop."
+        abort "mouse: The file $_ does not exist or is hidden. "
+        log -level "error" -opt $opt -status 404 -msg "ADD: Item $_ does not exist or is hidden."
     }
 }
 
@@ -129,18 +145,20 @@ if (test_internet) {
         git pull origin master --allow-unrelated-histories > "$psscriptroot\..\share\dump.tmp"
         git push origin master > "$psscriptroot\..\share\dump.tmp"
         info "Synchronized repository with GitHub."
+        log -level "success" -opt $opt -status 201 -msg "ADD: Synchronized repository with GitHub."
     }
     else {
         info "Option --nosync set, skipping synchronization."
     }
     success "Successfully added files."
+    log -level "success" -opt $opt -status 202 -msg "ADD: Completed command mouse add"
 }
 
 else {
     success "Successfully added files."
+    log -level "success" -opt $opt -status 202 -msg "ADD: Completed command mouse add"
     warn "Mouse was unable to push to GitHub; there does not appear to be an internet connection."
+    log -level "warn" -opt $opt -status 304 -msg "ADD: No internet connection, skipping synchronization with GitHub."
 }
 
-Set-Location $HOME/.mouse/dat
-git-crypt lock
 Pop-Location
